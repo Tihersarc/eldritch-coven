07d185d794b48b30b34f05c3006e6f52e9231d5e
David <davidsanzraya@gmail.com>
2024-01-30 20:20:27 +0100

    pathfinding

---
+++ EldritchCoven/Assets/EnemyAI/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
