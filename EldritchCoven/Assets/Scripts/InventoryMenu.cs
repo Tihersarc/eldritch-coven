7f332488ac3821a2e5707600f5fec17df6038170
quimilluuu <118997534+quimilluuu@users.noreply.github.com>
2024-02-27 19:25:59 +0100

    Side inventory done

---
+++ EldritchCoven/Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InventoryMenu : MonoBehaviour
+{
+    [SerializeField]
+    private float panelXPositionWhenOpen = 100;
+    [SerializeField]
+    private float panelXPositionWhenClosed = -100;
+    private float panelXPosition;
+    [SerializeField]
+    private float panelVelocity;
+
+    [SerializeField]
+    private RectTransform panelPosition;
+
+    private bool inventoryOpened = false;
+    private bool movePanel;
+
+    private void Start()
+    {
+        panelPosition.anchoredPosition = new Vector2(panelXPositionWhenClosed, 0);
+        panelXPosition = panelXPositionWhenClosed;
+    }
+
+    private void Update()
+    {
+        if (movePanel)
+        {
+            if (inventoryOpened)
+            {
+                panelXPosition += panelVelocity;
+                if (panelXPosition > panelXPositionWhenOpen)
+                {
+                    panelXPosition = panelXPositionWhenOpen;
+                    movePanel = false;
+                }
+            }
+            else
+            {
+                panelXPosition -= panelVelocity;
+                if (panelXPosition < panelXPositionWhenClosed)
+                {
+                    panelXPosition = panelXPositionWhenClosed;
+                    movePanel = false;
+                }
+            }
+            panelPosition.anchoredPosition = new Vector2(panelXPosition, 0);
+        }
+    }
+
+    private void OnOpenInventory(InputValue input)
+    {
+        if (inventoryOpened)
+        {
+            inventoryOpened = false;
+            movePanel = true;
+        }
+        else
+        {
+            inventoryOpened = true;
+            movePanel = true;
+        }
+    }
+}
