9ccf8629a7b8411e1a8397aa6ee020d21ff25d50
quimilluuu <118997534+quimilluuu@users.noreply.github.com>
2024-03-11 20:06:21 +0100

    Inventory advances

--- EldritchCoven/Assets/Scripts/Inventory.cs
+++ EldritchCoven/Assets/Scripts/Inventory.cs
@@ -5,24 +5,60 @@ using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
-    private List<GameObject> objetcsInInventory;
     [SerializeField]
+    private List<GameObject> objetcsInInventory;
+    private int currentItem;
     private Animator objectInHandAnim;
+    [SerializeField]
+    private GameObject hand;
+
+    private void Start()
+    {
+        objectInHandAnim = hand.GetComponent<Animator>();
+        currentItem = 0;
+    }
 
     public void AddItemToInventory(GameObject newObject)
     {
-
+        objetcsInInventory.Add(newObject);
     }
 
     public void RemoveItemFromInventory(GameObject objectToRemove)
     {
+        objetcsInInventory.Remove(objectToRemove);
+    }
 
+    public void NextItem()
+    {
+        Destroy(hand.transform.GetChild(0).GetComponent<GameObject>());
+        Instantiate(objetcsInInventory[currentItem], hand.transform);
     }
 
     public void OnChangeItem(InputValue input)
     {
         if (input.Get<Vector2>().y < 0)
         {
+            if (--currentItem < 0)
+            {
+                currentItem = objetcsInInventory.Count - 1;
+            }
+            else
+            {
+                currentItem--;
+            }
+            objectInHandAnim.SetTrigger("TriggerObject");
+        }
+
+        if (input.Get<Vector2>().y > 0)
+        {
+            if (++currentItem >= objetcsInInventory.Count)
+            {
+                currentItem = 0;
+            }
+            else
+            {
+                currentItem++;
+            }
             objectInHandAnim.SetTrigger("TriggerObject");
         }
     }
