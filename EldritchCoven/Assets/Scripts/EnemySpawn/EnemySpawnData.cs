625782bfed5588ca09fc58934603206f809b8ba4
DavidSanzRaya <dsanzraya@hotmail.com>
2024-03-12 20:36:26 +0100

    spawning enemies with default meshes

--- EldritchCoven/Assets/Scripts/EnemySpawn/EnemySpawnData.cs
+++ EldritchCoven/Assets/Scripts/EnemySpawn/EnemySpawnData.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+
 public class EnemySpawnData : MonoBehaviour
 {
     [SerializeField] Mesh[] propsMeshes;
     [SerializeField] Enemy[] enemies;
+    private void Start()
+    {
+        Quaternion enemyRotation = Quaternion.LookRotation(GameLogic.instance.playerController.transform.position - this.transform.position);
+        GameObject enemy = Instantiate(enemies[(int)Random.Range(0, propsMeshes.Length)].gameObject, this.gameObject.transform);
+        enemy.transform.position = this.transform.position;
+        enemy.transform.rotation = enemyRotation;
+    }
 
     public void Spawn()
     {
         this.GetComponent<MeshFilter>().mesh = propsMeshes[(int) Random.Range(0, propsMeshes.Length)];
 
         Quaternion enemyRotation = Quaternion.LookRotation(GameLogic.instance.playerController.transform.position - this.transform.position);
-        Instantiate(enemies[(int)Random.Range(0, propsMeshes.Length)].gameObject, this.transform.position, enemyRotation);
+        GameObject enemy = Instantiate(enemies[(int)Random.Range(0, propsMeshes.Length)].gameObject, this.gameObject.transform);
+        enemy.transform.rotation = enemyRotation;
     }
 
     private void OnEnable()
