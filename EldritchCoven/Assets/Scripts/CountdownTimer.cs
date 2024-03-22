dcac8a8ebfbe7814d7c461e17d50263a6a2d19dd
DavidSanzRaya <dsanzraya@hotmail.com>
2024-03-22 19:04:44 +0100

    navmesh and enemy instantiating

--- EldritchCoven/Assets/Scripts/CountdownTimer.cs
+++ EldritchCoven/Assets/Scripts/CountdownTimer.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CountdownTimer : MonoBehaviour
 {
     [SerializeField] float totalTime = 300f;
     float timeRemaining;
 
+    [SerializeField] GameObject monster;
+
+    [SerializeField] Transform mirrorCamera;
+    [SerializeField] Transform cameraEndPoint;
+    Vector3 initialCameraPosition;
+    [SerializeField] Transform[] spawnPoints;
+
+    public UnityEvent onTimeOut;
+
     private void Start()
     {
         timeRemaining = totalTime;
+        initialCameraPosition = mirrorCamera.position;
+    }
+
+    private void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            float fractionElapsed = 1 - (timeRemaining / totalTime);
+            mirrorCamera.position = Vector3.Lerp(initialCameraPosition, cameraEndPoint.position, fractionElapsed);
+        }
+        else
+        {
+            Instantiate(monster, spawnPoints[Random.Range(0, spawnPoints.Length - 1)].position, Quaternion.identity);
+            onTimeOut.Invoke();
+        }
     }
 }
