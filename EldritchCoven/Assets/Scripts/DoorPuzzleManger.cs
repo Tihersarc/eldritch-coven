8c50545d5f2e9ec8e83d37412b8939e486100f0b
quimilluuu <quimilluuu@gmail.com>
2024-05-12 21:26:16 +0200

    PuzzleSecondFloor

---
+++ EldritchCoven/Assets/Scripts/DoorPuzzleManger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorPuzzleManger : MonoBehaviour
+{
+    [SerializeField] List<int> order;
+
+    private List<int> currentOrder;
+
+    private bool canInputNewNumber;
+    public UnityEvent onCorrectSequence;
+
+    public void AddNumberToCurrentSequence(int newNumber)
+    {
+        if (canInputNewNumber)
+        {
+            bool equals = true;
+            currentOrder.Add(newNumber);
+            if (currentOrder.Count == order.Count)
+            {
+                for (int i = 0; i < order.Count; i++)
+                {
+                    if (!(order[i] == currentOrder[i]))
+                    {
+                        equals = false;
+                    }
+                }
+                if (equals)
+                {
+                    onCorrectSequence.Invoke();
+                }
+                else
+                {
+                    currentOrder.RemoveAt(0);
+                }
+            }
+            canInputNewNumber = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canInputNewNumber = true;
+    }
+}
